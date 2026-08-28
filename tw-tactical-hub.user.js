9af5f689811b216641b38148d9dc1a3002a50515
jura75 <jurabashmakov@gmail.com>
2026-08-29 02:36:34 +0300

    Fix IIFE syntax in tw-tactical-hub.user.js

--- tw-tactical-hub.user.js
+++ tw-tactical-hub.user.js
@@ -1,4 +1,4 @@
-(function() {
+javascript:(function() {
     let existingPanel = document.getElementById('custom-tactical-hub');
     if (existingPanel) {
         if (existingPanel.style.display === 'none') {
@@ -30,7 +30,6 @@
     let savedMaxSource = localStorage.getItem('tw_hub_tc_max_source') || '1';
 
     let currentActiveTab = localStorage.getItem('tw_hub_active_tab') || 'incomings';
-    // Если раньше были сохранены удаленные вкладки, сбрасываем на 'incomings'
     if (['hub', 'tribe', 'stats'].includes(currentActiveTab)) {
         currentActiveTab = 'incomings';
     }
